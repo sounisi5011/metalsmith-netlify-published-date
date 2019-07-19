597edc93f4f062e0d4508be8fb0e93975e5301a6
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-07-20 00:40:26 +0900

    ✅ Add test of netlifyDeploys function

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -1,3 +1,13 @@
+export function deleteProps<T extends Record<U, unknown>, U extends string>(
+    { ...obj }: T,
+    props: readonly U[],
+): Omit<T, U> {
+    props.forEach(prop => {
+        delete obj[prop];
+    });
+    return obj;
+}
+
 export function addSlash(path: string): string {
     return path.startsWith('/') ? path : `/${path}`;
 }
---
+++ test/netlify.ts
@@ -0,0 +1,53 @@
+import anyTest, { TestInterface } from 'ava';
+
+import { netlifyDeploys } from '../src/netlify';
+import createNetlify from './helpers/netlify-mock-server';
+import { deleteProps } from './helpers/utils';
+
+const test = anyTest as TestInterface<{
+    server: ReturnType<typeof createNetlify>;
+}>;
+
+test.before(t => {
+    t.context.server = createNetlify('example.com');
+});
+
+test('netlifyDeploys(): If the commitHashList option is undefined, all deploys should be return', async t => {
+    const server = await t.context.server;
+
+    const deployList = await netlifyDeploys('example.com', {
+        commitHashList: undefined,
+    });
+
+    t.deepEqual(
+        deployList.map(deploy => deleteProps(deploy, ['deployAbsoluteURL'])),
+        [...server.deploys],
+    );
+});
+
+test('netlifyDeploys(): If the commitHashList option is an empty array, an empty array must be returned', async t => {
+    await t.context.server;
+
+    const deployList = await netlifyDeploys('example.com', {
+        commitHashList: [],
+    });
+
+    t.deepEqual(deployList, []);
+});
+
+test('netlifyDeploys(): If commit hash is specified in commitHashList option, it is necessary to return two of target deploy and initial deploy', async t => {
+    const server = await t.context.server;
+    const deploy =
+        server.deploys.find(
+            deploy => deploy.commit_ref && deploy !== server.deploys.initial,
+        ) || server.deploys.modified;
+
+    const deployList = await netlifyDeploys('example.com', {
+        commitHashList: [deploy.commit_ref || ''],
+    });
+
+    t.deepEqual(
+        deployList.map(deploy => deleteProps(deploy, ['deployAbsoluteURL'])),
+        [deploy, server.deploys.initial],
+    );
+});
