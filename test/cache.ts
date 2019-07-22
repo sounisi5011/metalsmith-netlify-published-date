24b5635be0bda8de0f231c2c25c7937d2ff5d790
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-07-22 10:26:41 +0900

    ✅ Add test for cache
    * Cache should be shared within one Node.js process
    * Should not delete cache entries that were not read

--- test/cache.ts
+++ test/cache.ts
@@ -36,7 +36,7 @@ function processEachGenPlugin<T>(
 test('filesystem', async t => {
     const siteID = 'filesystem.cache.test';
     const metalsmith = Metalsmith(path.join(fixtures, 'basic'));
-    const cacheDir = path.join(metalsmith.directory(), 'cache');
+    const cacheDir = path.join(metalsmith.directory(), 'cache', siteID);
 
     await del(cacheDir);
 
@@ -97,6 +97,11 @@ test('filesystem', async t => {
         firstApiLogs,
         'API requests should not be cached',
     );
+    t.notDeepEqual(
+        secondPreviewsLogs,
+        firstPreviewsLogs,
+        'cache should be shared within each Node.js process',
+    );
     t.deepEqual(
         secondPreviewsLogs,
         firstPreviewsLogs.filter(requestLog => requestLog.statusCode === 404),
@@ -126,7 +131,7 @@ test('in-memory', async t => {
     const metalsmith = Metalsmith(path.join(fixtures, 'basic'))
         .use(stripFileStatsPlugin())
         .use(
-            netlifyPublishedDate({
+            processEachGenPlugin(netlifyPublishedDate, {
                 siteID,
                 cacheDir: null,
                 defaultDate: new Date(),
@@ -176,9 +181,135 @@ test('in-memory', async t => {
         firstApiLogs,
         'API requests should not be cached',
     );
+    t.notDeepEqual(
+        secondPreviewsLogs,
+        firstPreviewsLogs,
+        'cache should be shared within each Node.js process',
+    );
     t.deepEqual(
         secondPreviewsLogs,
         firstPreviewsLogs.filter(requestLog => requestLog.statusCode === 404),
+        'should not fetch anything other than a preview that returned 404 Not Found',
+    );
+
+    t.deepEqual(
+        thirdFiles,
+        firstFiles,
+        'generated files metadata should be the same',
+    );
+    t.notDeepEqual(thirdApiLogs, [], 'API requests should not be cached');
+    t.deepEqual(
+        thirdApiLogs,
+        firstApiLogs,
+        'API requests should not be cached',
+    );
+    t.deepEqual(
+        thirdPreviewsLogs,
+        firstPreviewsLogs.filter(requestLog => requestLog.statusCode === 404),
+        'should not fetch anything other than a preview that returned 404 Not Found',
+    );
+});
+
+test('filesystem: unread cache entries should also be kept', async t => {
+    const siteID = 'unread-cache-entries-kept.cache.test';
+    const defaultDate = new Date();
+    const metalsmith = Metalsmith(path.join(fixtures, 'basic'));
+    const cacheDir = path.join(metalsmith.directory(), 'cache', siteID);
+    const server = await createNetlify(siteID, {
+        root: metalsmith.source(),
+        initial: 'initial.html',
+        modified: 'modified.html',
+        added: 'added.html',
+    });
+
+    let firstFiles: Metalsmith.Files = {};
+    let firstApiLogs: typeof server.requestLogs.api = [];
+    let firstApiLogLen = 0;
+    let firstPreviewsLogs: typeof server.requestLogs.previews = [];
+    let firstPreviewsLogLen = 0;
+
+    let secondFiles: Metalsmith.Files = {};
+    let secondApiLogs: typeof server.requestLogs.api = [];
+    let secondApiLogLen = 0;
+    let secondPreviewsLogs: typeof server.requestLogs.previews = [];
+    let secondPreviewsLogLen = 0;
+
+    await del(cacheDir);
+
+    const thirdFiles = await util.promisify(
+        metalsmith
+            .use(
+                netlifyPublishedDate({
+                    siteID,
+                    cacheDir,
+                    defaultDate,
+                }),
+            )
+            .use((files, metalsmith, done) => {
+                const { api, previews } = server.requestLogs;
+                firstFiles = files;
+                firstApiLogs = [...api];
+                firstApiLogLen = api.length;
+                firstPreviewsLogs = [...previews];
+                firstPreviewsLogLen = previews.length;
+                done(null, files, metalsmith);
+            })
+            .use(
+                netlifyPublishedDate({
+                    pattern: 'modified.html',
+                    siteID,
+                    cacheDir,
+                    defaultDate,
+                }),
+            )
+            .use((files, metalsmith, done) => {
+                const { api, previews } = server.requestLogs;
+                secondFiles = files;
+                secondApiLogs = api.slice(firstApiLogLen);
+                secondApiLogLen = api.length;
+                secondPreviewsLogs = previews.slice(firstPreviewsLogLen);
+                secondPreviewsLogLen = previews.length;
+                done(null, files, metalsmith);
+            })
+            .use(
+                netlifyPublishedDate({
+                    siteID,
+                    cacheDir,
+                    defaultDate,
+                }),
+            )
+            .process.bind(metalsmith),
+    )();
+    const thirdApiLogs = server.requestLogs.api.slice(secondApiLogLen);
+    const thirdPreviewsLogs = server.requestLogs.previews.slice(
+        secondPreviewsLogLen,
+    );
+
+    const modifiedOnlyFirstPreviewsLogs = firstPreviewsLogs.filter(
+        requestLog => requestLog.path === '/modified.html',
+    );
+
+    t.deepEqual(
+        secondFiles,
+        firstFiles,
+        'generated files metadata should be the same',
+    );
+    t.notDeepEqual(secondApiLogs, [], 'API requests should not be cached');
+    t.deepEqual(
+        secondApiLogs,
+        firstApiLogs,
+        'API requests should not be cached',
+    );
+    t.notDeepEqual(
+        secondPreviewsLogs,
+        modifiedOnlyFirstPreviewsLogs,
+        'cache should be shared within each Node.js process',
+    );
+    t.deepEqual(
+        secondPreviewsLogs,
+        modifiedOnlyFirstPreviewsLogs.filter(
+            requestLog => requestLog.statusCode === 404,
+        ),
         'should not fetch anything other than a preview that returned 404 Not Found',
     );
 
