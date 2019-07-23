52c4a8ef2f3a7c1c40e3691d45591b09d8bd4f9b
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-07-24 02:38:50 +0900

    🔥 Remove extra t.log() methods
    The test content has become so simple that it is no longer necessary to output the results of command execution to a log when the test succeeds.

--- test/cli.ts
+++ test/cli.ts
@@ -9,30 +9,19 @@ const PROJECT_ROOT = path.resolve(__dirname, '..');
 
 test.before(async t => {
     if (!(await fileExists(PROJECT_ROOT, 'dist', 'index.js'))) {
-        t.log(
-            await exec(['npm', 'run', 'build'], {
-                cwd: PROJECT_ROOT,
-            }),
-        );
+        await exec(['npm', 'run', 'build'], {
+            cwd: PROJECT_ROOT,
+        });
     }
 });
 
 test('should work with Metalsmith CLI', async t => {
-    await t.notThrowsAsync(async () => {
-        t.log(
-            await exec(
-                [
-                    path.resolve(
-                        PROJECT_ROOT,
-                        'node_modules',
-                        '.bin',
-                        'metalsmith',
-                    ),
-                ],
-                {
-                    cwd: path.join(fixtures, 'cli'),
-                },
-            ),
-        );
-    });
+    await t.notThrowsAsync(
+        exec(
+            [path.resolve(PROJECT_ROOT, 'node_modules', '.bin', 'metalsmith')],
+            {
+                cwd: path.join(fixtures, 'cli'),
+            },
+        ),
+    );
 });
