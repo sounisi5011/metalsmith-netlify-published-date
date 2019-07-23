81f3d12ae058dc36001691de1520a0fccccfebba
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-07-24 00:50:57 +0900

    🎨 Build the contents of the example directory with Metalsmith CLI
    Added configuration file of Metalsmith CLI and changed the configuration to generate Netlify build in CLI.

--- example/metalsmith.js
+++
@@ -1,9 +0,0 @@
-const metalsmith = require('metalsmith');
-
-metalsmith(__dirname)
-    .build(err => {
-        if (err) {
-            process.exitCode = 1;
-            throw err;
-        }
-    });
