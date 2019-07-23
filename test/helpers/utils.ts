fe288e9b4248aacb4fd16f20ece655b3f564b9f2
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-07-24 02:34:42 +0900

    🔥 Remove files and code added for testing

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import util from 'util';
 
 const fsStat = util.promisify(fs.stat);
-const fsReadFile = util.promisify(fs.readFile);
 
 export function hasProp(
     value: unknown,
@@ -28,13 +27,6 @@ export function addSlash(path: string): string {
     return path.startsWith('/') ? path : `/${path}`;
 }
 
-/**
- * @see https://qiita.com/muddydixon/items/2edf6dcb84295eccf4f3
- */
-export function isValidDate(date: Date): boolean {
-    return !Number.isNaN(date.getTime());
-}
-
 export async function fileExists(...paths: string[]): Promise<boolean> {
     try {
         await fsStat(path.resolve(...paths));
@@ -45,8 +37,4 @@ export async function fileExists(...paths: string[]): Promise<boolean> {
         }
         throw err;
     }
-}
-
-export async function readFile(...paths: string[]): Promise<string> {
-    return fsReadFile(path.resolve(...paths), 'utf8');
 }
