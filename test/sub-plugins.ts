1df29f7437a3726dc9008eedda76e16820e6175f
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-05 03:20:02 +0900

    ✅ Add test to check the date of metadata of "initial.html" file to test code of "plugins" option

--- test/sub-plugins.ts
+++ test/sub-plugins.ts
@@ -138,14 +138,14 @@ test('Plugins specified in the "plugins" option should be execute', async t => {
         'mustache template files should be converted to html',
     );
 
-    // t.deepEqual(
-    //     files['initial.html'].published,
-    //     new Date(initialDeploy.published_at || initialDeploy.created_at),
-    // );
-    // t.deepEqual(
-    //     files['initial.html'].modified,
-    //     new Date(initialDeploy.published_at || initialDeploy.created_at),
-    // );
+    t.deepEqual(
+        files['initial.html'].published,
+        new Date(initialDeploy.published_at || initialDeploy.created_at),
+    );
+    t.deepEqual(
+        files['initial.html'].modified,
+        new Date(initialDeploy.published_at || initialDeploy.created_at),
+    );
 
     // t.true(files['new.html'].published instanceof Date);
     // t.true(files['new.html'].published > lastPublishedDate);
@@ -287,14 +287,14 @@ test('If the plugin gets progressing build of self, make the published date and 
         'mustache template files should be converted to html',
     );
 
-    // t.deepEqual(
-    //     files['initial.html'].published,
-    //     new Date(initialDeploy.published_at || initialDeploy.created_at),
-    // );
-    // t.deepEqual(
-    //     files['initial.html'].modified,
-    //     new Date(initialDeploy.published_at || initialDeploy.created_at),
-    // );
+    t.deepEqual(
+        files['initial.html'].published,
+        new Date(initialDeploy.published_at || initialDeploy.created_at),
+    );
+    t.deepEqual(
+        files['initial.html'].modified,
+        new Date(initialDeploy.published_at || initialDeploy.created_at),
+    );
 
     // t.deepEqual(
     //     files['new.html'].published,
