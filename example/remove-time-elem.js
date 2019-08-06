0202390d7b7b26617ec0858556643aed814af3a1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-06 18:30:24 +0900

    🔧 Add "contentsConverter" option to "metalsmith.json" in example directory
    Added a script to delete child elements of "time" element and "datetime" attribute value of "time" element.

---
+++ example/remove-time-elem.js
@@ -0,0 +1,24 @@
+const cheerio = require('cheerio');
+
+module.exports = contents => {
+  try {
+    const $ = cheerio.load(contents.toString());
+
+    $timeElems = $('time');
+
+    // Note: If the file contents is not valid HTML, cheerio will not throw an error.
+    //       However, the number of detected "time" elements will be 0.
+    if (1 <= $timeElems.length) {
+      $timeElems.each((index, element) => {
+        $time = $(element);
+        $time.empty();
+        if ($time.is('[datetime]')) {
+          $time.attr('datetime', '');
+        }
+      });
+      return Buffer.from($.html());
+    }
+  } finally {
+    return contents;
+  }
+};
