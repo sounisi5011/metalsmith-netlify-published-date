6937715e2d40d793fca0918492a8dabee8ca75fc
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-09-06 18:55:00 +0900

    🔧 Add "metadataUpdater" option to "metalsmith.json" in example directory
    Added a script to delete child elements of "time" element and "datetime" attribute value of "time" element.

---
+++ example/set-datetime.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio');
+
+function isValidDate(date) {
+  return !isNaN(date.getTime());
+}
+
+function getTime($time) {
+  const dateStr = $time.attr('datetime') || $time.text();
+  if (dateStr) {
+    const date = new Date(dateStr);
+    if (isValidDate(date)) {
+      return date;
+    }
+  }
+  return null;
+}
+
+module.exports = (previewContents, filedata) => {
+  try {
+    const $ = cheerio.load(previewContents.toString());
+
+    const b = { ...filedata };
+
+    const publishedDate = getTime($('time.published'));
+    if (publishedDate) {
+      filedata.published = publishedDate;
+    }
+
+    const modifiedDate = getTime($('time.modified'));
+    if (modifiedDate) {
+      filedata.modified = modifiedDate;
+    }
+  } catch (err) {}
+};
